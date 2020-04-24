6aa7b86793b5cfa0e21e59f0cd22437cf018f77f
siekiera-a <siekiera.a31@gmail.com>
2020-04-24 02:16:54 +0200

    Recompiled MediaKeys class with changed method name. Added check if OS is Windows

--- src/java/remoteserver/SystemControl/MediaKeys.java
+++ src/java/remoteserver/SystemControl/MediaKeys.java
@@ -1,21 +1,27 @@
 package remoteserver.SystemControl;
 
-/**
- * @TODO recompile C file with correct function name
- */
 public final class MediaKeys {
 
-    private MediaKeys() {}
+    private MediaKeys() {
+    }
 
     static {
-        System.load(System.getProperty("user.dir") + "\\src\\native\\MediaKeys.dll");
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.load(System.getProperty("user.dir") + "\\src\\native\\MediaKeys.dll");
+        }
     }
 
     public static native void volumeUp();
-    public static native void VolumeDown();
+
+    public static native void volumeDown();
+
     public static native void mute();
+
     public static native void mediaNext();
+
     public static native void mediaPrevious();
+
     public static native void mediaPlayPause();
+
     public static native void mediaStop();
 }
--- src/native/MediaKeys.c
+++ src/native/MediaKeys.c
@@ -30,30 +30,30 @@ void click(WORD VK) {
 	press_key = NULL;
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_volumeUp(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_volumeUp(JNIEnv *env, jobject obj) {
 	click(VK_VOLUME_UP);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_VolumeDown(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_volumeDown(JNIEnv *env, jobject obj) {
 	click(VK_VOLUME_DOWN);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_mute(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_mute(JNIEnv *env, jobject obj) {
 	click(VK_VOLUME_MUTE);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_mediaNext(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_mediaNext(JNIEnv *env, jobject obj) {
 	click(VK_MEDIA_NEXT_TRACK);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_mediaPrevious(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_mediaPrevious(JNIEnv *env, jobject obj) {
 	click(VK_MEDIA_PREV_TRACK);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_mediaPlayPause(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_mediaPlayPause(JNIEnv *env, jobject obj) {
 	click(VK_MEDIA_PLAY_PAUSE);
 }
 
-JNIEXPORT void JNICALL Java_org_remoteserver_MediaKeys_mediaStop(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_remoteserver_SystemControl_MediaKeys_mediaStop(JNIEnv *env, jobject obj) {
 	click(VK_MEDIA_STOP);
 }
