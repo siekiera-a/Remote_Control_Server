35a04270d7d7e2c86c33d12cf257794e720d71cb
siekiera-a <siekiera.a31@gmail.com>
2020-04-24 19:37:50 +0200

    SystemInput refactor (Singleton)

--- src/java/remoteserver/SystemControl/SystemInput.java
+++ src/java/remoteserver/SystemControl/SystemInput.java
@@ -5,52 +5,73 @@ import java.awt.AWTKeyStroke;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
+
 import remoteserver.SystemControl.exceptions.RobotException;
 
 import java.awt.event.InputEvent;
-
+import java.util.Arrays;
 
 /**
-    This class allows you to emit user input from keyboard and mouse
+ * This class allows you to emit user input from keyboard and mouse
  */
-public class SystemInput {
+public final class SystemInput {
 
+    private static SystemInput systemInput;
 
-    private Robot robot = null;
+    private final Robot robot;
 
     public enum MouseButton {
         LPM, PPM, SCROLL_WHEEL
     }
 
     public enum Shortcut {
-        CTRL_Z, CTRL_X, CTRL_C, CTRL_V, CTRL_A, CTRL_D, CTRL_Y, CTRL_S, CTRL_P, ALT_F4, WINDOWS_D, SHIFT_DELETE
+        CTRL_Z,
+        CTRL_X,
+        CTRL_C,
+        CTRL_V,
+        CTRL_A,
+        CTRL_D,
+        CTRL_Y,
+        CTRL_S,
+        CTRL_P,
+        ALT_F4,
+        WINDOWS_D,
+        SHIFT_DELETE
     }
 
     /**
-     * Create class Robot instance
-     * @throws RobotException if system does not allow to control input or you have not permission
+     * @throws RobotException if system does not allow to control input or you have not permissions
      */
-    public SystemInput() throws RobotException  {
+    private SystemInput() throws RobotException {
         try {
             robot = new Robot();
-        }
-        catch(AWTException e) {
+        } catch (AWTException e) {
             throw new RobotException("System does not allow to control input", RobotException.ErrorCode.NOT_ALLOWED);
-        }
-        catch (SecurityException e) {
-            throw new RobotException("You have not permission to control input", RobotException.ErrorCode.NO_PERMISSION);
+        } catch (SecurityException e) {
+            throw new RobotException("You have not permissions to control input", RobotException.ErrorCode.NO_PERMISSION);
         }
     }
 
+    /**
+     * Get instance of SystemInput class
+     *
+     * @throws RobotException if system does not allow to control input or you have not permissions
+     */
+    public static SystemInput getInstance() throws RobotException {
+        if (systemInput == null) {
+            systemInput = new SystemInput();
+        }
+
+        return systemInput;
+    }
 
     /**
-     Press and Release Key from keyboard
-     @param keyCode Int from KeyEvent (e.g. KeyEvent.VK_A)
-     @return true if keyCode is valid, false otherwise
-    */
+     * Press and release keyboard key
+     *
+     * @param keyCode (e.g. KeyEvent.VK_A)
+     * @return true if keyCode is valid, false otherwise
+     */
     public boolean clickKey(int keyCode) {
-        if (robot == null) return false;
-
         try {
             robot.keyPress(keyCode);
             robot.keyRelease(keyCode);
@@ -61,40 +82,9 @@ public class SystemInput {
     }
 
     /**
-     * Use shortcut from list
+     * Press and release mouse key
      */
-    public void useShortcut(Shortcut shortcut) {
-        if(robot == null) return;
-
-        String[] keys = shortcut.name().toUpperCase().split("_");
-        if (keys[0].equals("CTRL")) keys[0] = "CONTROL";
-        int[] keyCodes = new int[keys.length];
-        for (int i = 0; i < keyCodes.length; i++) {
-            keyCodes[i] = AWTKeyStroke.getAWTKeyStroke(keys[i]).getKeyCode();
-        }
-        pressKeys(keyCodes);
-        releaseKeys(keyCodes);
-    }
-
-    private void pressKeys(int[] keys) {
-        if (robot == null) return;
-
-        for (int key : keys) {
-            robot.keyPress(key);
-        }
-    }
-
-    private void releaseKeys(int[] keys) {
-        if (robot == null) return;
-
-        for (int key : keys) {
-            robot.keyRelease(key);
-        }
-    }
-
     public void mouseClick(MouseButton button) {
-        if (robot == null) return;
-
         int buttonKey;
 
         if (button == MouseButton.LPM) {
@@ -110,39 +100,39 @@ public class SystemInput {
     }
 
     /**
-     * @param button 1 = LPM, 2 = SCROLL_WHEEL, 3 = PPM
+     * Use shortcut from list
      */
-    public void mouseClick(int button) {
-        if (robot == null) return;
+    public void useShortcut(Shortcut shortcut) {
+        String[] keys = shortcut.name()
+            .replace("CTRL", "CONTROL")
+            .split("_");
 
-        int buttonKey;
-        switch (button) {
-            case 1:
-                buttonKey = InputEvent.BUTTON1_DOWN_MASK;
-                break;
-            case 2:
-                buttonKey = InputEvent.BUTTON2_DOWN_MASK;
-                break;
-            case 3:
-                buttonKey = InputEvent.BUTTON3_DOWN_MASK;
-                break;
-            default:
-                buttonKey = 0;
-        }
+        int[] keyCodes = Arrays.stream(keys)
+            .mapToInt(key -> AWTKeyStroke.getAWTKeyStroke(key).getKeyCode())
+            .toArray();
 
-        if (buttonKey != 0) {
-            robot.mousePress(buttonKey);
-            robot.mouseRelease(buttonKey);
-        }
+        pressKeys(keyCodes);
+        releaseKeys(keyCodes);
     }
 
     /**
-     * Moves mouse from current location by x, y
+     * Move mouse pointer from current location by x, y
      */
     public void mouseMove(int x, int y) {
-        if (robot == null) return;
         Point currentPosition = MouseInfo.getPointerInfo().getLocation();
-        robot.mouseMove(x + currentPosition.x,y + currentPosition.y);
+        robot.mouseMove(x + currentPosition.x, y + currentPosition.y);
+    }
+
+    private void pressKeys(int[] keys) {
+        for (int key : keys) {
+            robot.keyPress(key);
+        }
+    }
+
+    private void releaseKeys(int[] keys) {
+        for (int key : keys) {
+            robot.keyRelease(key);
+        }
     }
 
 }
